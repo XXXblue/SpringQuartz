a2b0c3c73def48d2576b0d840ce350066a84ba6d
XXXblue <1123049661@qq.com>
2018-05-13 17:33:08 +0800

    该版本融合增删改暂停以及继续的触发。。。。

--- src/main/java/com/test/TestController.java
+++ src/main/java/com/test/TestController.java
@@ -1,5 +1,7 @@
 package com.test;
 
+import com.service.QuartzService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,10 +14,61 @@ import org.springframework.web.bind.annotation.ResponseBody;
  */
 @Controller
 public class TestController {
+    @Autowired
+    private QuartzService quartzService;
 
     @RequestMapping("/test")
     @ResponseBody
-    public String test(){
+    public String test()
+    {
+        return "abc";
+    }
+
+    @RequestMapping("/add")
+    @ResponseBody
+    public String add()
+    {
+        quartzService.addJob("jobName1","jobGroupName"
+        ,"triggerName1","triggerGroupName1",MyJob.class,"0/2 * * * * ?");
+        return "abc";
+    }
+
+    @RequestMapping("delete")
+    @ResponseBody
+    public String remove(){
+        quartzService.removeJob("jobName1","jobGroupName",
+                "triggerName1","triggerGroupName1");
+        return "abc";
+    }
+
+    @RequestMapping("stop")
+    @ResponseBody
+    public String stop(){
+        quartzService.pauseJob("jobName1","jobGroupName");
+        return "abc";
+    }
+
+    @RequestMapping("restart")
+    @ResponseBody
+    public String restart(){
+        quartzService.resumeJob("jobName1","jobGroupName");
+        return "abc";
+    }
+
+    @RequestMapping("modify")
+    @ResponseBody
+    public String modify(){
+        quartzService.modifyJobTime("jobName1","jobGroupName",
+                "triggerName1","triggerGroupName1","jobName2","jobGroupName",
+                "triggerName2","triggerGroupName1","0/5 * * * * ?");
+        return "abc";
+    }
+
+    @RequestMapping("delete2")
+    @ResponseBody
+    public String delete2(){
+        quartzService.removeJob("jobName2","jobGroupName",
+                "triggerName2","triggerGroupName1");
         return "abc";
     }
 }
